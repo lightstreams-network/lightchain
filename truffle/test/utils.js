f2ac98784eb4e48203894bf8725e803a72fab763
ggarri <gabriel.garrido.work@gmail.com>
2019-01-06 13:47:44 +0100

    [#4] Improve truffle test granularity to better define test scopes

--- truffle/test/utils.js
+++ truffle/test/utils.js
@@ -1,42 +1,52 @@
-module.exports.convertFromWeiBnToEth = function (bn) {
+module.exports.convertFromWeiBnToPht = function(bn) {
   return Number(web3._extend.utils.fromWei(bn.toNumber(), 'ether'));
 };
 
-module.exports.convertEtherToWeiBN = function (ether) {
+module.exports.convertPhtToWeiBN = function(ether) {
   const etherInWei = web3._extend.utils.toWei(ether, 'ether');
   return web3._extend.utils.toBigNumber(etherInWei);
 };
 
-module.exports.fetchTxReceipt = function(txReceiptId, timeoutInSec = 30) {
-    const startTime = new Date();
-    const retryInSec = 2;
-    
-    const waitTime = function(waitInSec) {
-        return new Promise((resolve) => {
-            setTimeout(resolve, waitInSec * 1000)
-        })
-    };
+module.exports.gasToWei = function(gasAmount) {
+  return gasAmount * web3.eth.gasPrice.toNumber();
+};
 
-    console.log(`Fetching tx ${txReceiptId}`);
-    
-    return new Promise(async (resolve, reject) => {
-        while (true) {
-            let txReceipt = web3.eth.getTransactionReceipt(txReceiptId);
-            if (txReceipt != null && typeof txReceipt !== 'undefined'){
-                console.log('Receipt found');
-                resolve(txReceipt);
-                return;
-            }
-    
-            const now = new Date();
-            if (now.getTime() - startTime.getTime() > timeoutInSec * 1000) {
-                console.log('Receipt not found');
-                reject(`Timeout after ${timeoutInSec} seconds`);
-                return;
-            }
-            
-            console.log(`Now: ${now}, StartTime: ${startTime}...retrying in ${retryInSec}`);
-            await waitTime(retryInSec)
-        }
-    });
+module.exports.fetchTxReceipt = function(txReceiptId, timeoutInSec = 30) {
+  const startTime = new Date();
+  const retryInSec = 2;
+
+  const waitTime = function(waitInSec) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, waitInSec * 1000)
+    })
+  };
+
+  console.log(`Fetching tx ${txReceiptId}`);
+
+  return new Promise(async (resolve, reject) => {
+    while ( true ) {
+      let txReceipt = web3.eth.getTransactionReceipt(txReceiptId);
+      if (txReceipt != null && typeof txReceipt !== 'undefined') {
+        console.log('Receipt found');
+        resolve(txReceipt);
+        return;
+      }
+
+      const now = new Date();
+      if (now.getTime() - startTime.getTime() > timeoutInSec * 1000) {
+        console.log('Receipt not found');
+        reject(`Timeout after ${timeoutInSec} seconds`);
+        return;
+      }
+
+      console.log(`Now: ${now.toISOString()}, StartTime: ${startTime.toISOString()}...retrying in ${retryInSec} seconds`);
+      await waitTime(retryInSec)
+    }
+  });
+};
+
+module.exports.waitFor = (waitInSeconds) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, waitInSeconds * 1000);
+  });
 };
