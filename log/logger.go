028407a21b7fba62f19c860a0b034ae6c2894695
ggarri <gabriel.garrido.work@gmail.com>
2019-01-11 15:02:19 +0100

    [#3] Update logger class

--- log/logger.go
+++ log/logger.go
@@ -15,47 +15,47 @@ func SetupLogger(level log.Lvl) {
 }
 
 // Interface assertions
-var _ tmtLog.Logger = (*logger)(nil)
+var _ tmtLog.Logger = (*Logger)(nil)
 
 // ---------------------------
-// Logger - wraps the logger in tmlibs
+// Logger - wraps the Logger in tmlibs
 
-type logger struct {
+type Logger struct {
 	keyvals []interface{}
 }
 
-// Logger returns a new instance of an lightchain logger. With() should
+// Logger returns a new instance of an lightchain Logger. With() should
 // be called upon the returned instance to set default keys
 // #unstable
-func NewLogger() tmtLog.Logger {
-	logger := logger{keyvals: make([]interface{}, 0)}
+func NewLogger() Logger {
+	logger := Logger{keyvals: make([]interface{}, 0)}
 	return logger
 }
 
 // Debug proxies everything to the go-ethereum logging facilities
 // #unstable
-func (l logger) Debug(msg string, ctx ...interface{}) {
+func (l Logger) Debug(msg string, ctx ...interface{}) {
 	ctx = append(l.keyvals, ctx...)
 	log.Debug(msg, ctx...)
 }
 
 // Info proxies everything to the go-ethereum logging facilities
 // #unstable
-func (l logger) Info(msg string, ctx ...interface{}) {
+func (l Logger) Info(msg string, ctx ...interface{}) {
 	ctx = append(l.keyvals, ctx...)
 	log.Info(msg, ctx...)
 }
 
 // Error proxies everything to the go-ethereum logging facilities
 // #unstable
-func (l logger) Error(msg string, ctx ...interface{}) {
+func (l Logger) Error(msg string, ctx ...interface{}) {
 	ctx = append(l.keyvals, ctx...)
 	log.Error(msg, ctx...)
 }
 
 // With proxies everything to the go-ethereum logging facilities
 // #unstable
-func (l logger) With(ctx ...interface{}) tmtLog.Logger {
+func (l Logger) With(ctx ...interface{}) tmtLog.Logger {
 	l.keyvals = append(l.keyvals, ctx...)
 	return l
 }
