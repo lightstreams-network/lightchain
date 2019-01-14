a3bd8ab06129f259bdc360f22cbc0bdde754eef6
Lukas Lukac <lukas.lukac.io@gmail.com>
2019-01-14 15:59:06 +0100

    Moves the ethereum cli ctx and its flags to its own file.

--- cmd/lightchain/main.go
+++ cmd/lightchain/main.go
@@ -3,12 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"gopkg.in/urfave/cli.v1"
 	"github.com/spf13/cobra"
-	"flag"
-	
-	ethUtils "github.com/ethereum/go-ethereum/cmd/utils"
 	"github.com/lightstreams-network/lightchain/log"
 )
 
@@ -43,47 +38,8 @@ func LightchainCmd() *cobra.Command {
 }
 
 func addDefaultFlags(cmd *cobra.Command) {
+	cmd.Flags().String(DataDirFlag.Name, DataDirFlag.Value.Value, DataDirFlag.Usage)
 	cmd.Flags().String(LogLvlFlag.Name, LogLvlFlag.Value, LogLvlFlag.Usage)
 
-	cmd.Flags().String(DataDirFlag.Name, DataDirFlag.Value.Value, DataDirFlag.Usage)
 	cmd.MarkFlagRequired(DataDirFlag.Name)	
-}
-
-func NewNodeClientCtx(dataDir string, cmd *cobra.Command) *cli.Context {
-	var boolEmptyBucket bool
-	var stringEmptyBucket string
-	
-	app := ethUtils.NewApp("0.0.0", "the lightchain command line interface")
-	flagSet := flag.NewFlagSet("FakeCli", flag.ExitOnError)
-	ctx := cli.NewContext(app, flagSet, nil)
-	
-	flagSet.StringVar(&stringEmptyBucket, ethUtils.DataDirFlag.GetName(), dataDir, ethUtils.DataDirFlag.Usage)
-
-	rpcEnabledFlagValue, _ := cmd.Flags().GetBool(RPCEnabledFlag.GetName())
-	flagSet.BoolVar(&boolEmptyBucket, RPCEnabledFlag.GetName(), rpcEnabledFlagValue, RPCEnabledFlag.Usage)
-	
-	rpcListenAddrValue, _ := cmd.Flags().GetString(RPCListenAddrFlag.GetName())
-	flagSet.StringVar(&stringEmptyBucket, RPCListenAddrFlag.GetName(), rpcListenAddrValue, RPCListenAddrFlag.Usage)
-	
-	rpcPortFlagValue, _ := cmd.Flags().GetString(RPCPortFlag.GetName())
-	flagSet.StringVar(&stringEmptyBucket, RPCPortFlag.GetName(), rpcPortFlagValue, RPCPortFlag.Usage)
-	
-	rpcApiFlag, _ := cmd.Flags().GetString(RPCApiFlag.GetName())
-	flagSet.StringVar(&stringEmptyBucket, RPCApiFlag.GetName(), rpcApiFlag, RPCApiFlag.Usage)
-	
-	wsEnabledFlagValue, _ := cmd.Flags().GetBool(WSEnabledFlag.GetName())
-	flagSet.BoolVar(&boolEmptyBucket, WSEnabledFlag.GetName(), wsEnabledFlagValue, WSEnabledFlag.Usage)
-	
-	wsListenAddrFlag, _ := cmd.Flags().GetString(WSListenAddrFlag.GetName())
-	flagSet.StringVar(&stringEmptyBucket, WSListenAddrFlag.GetName(), wsListenAddrFlag, WSListenAddrFlag.Usage)
-	
-	wsPortFlag, _ := cmd.Flags().GetString(WSPortFlag.GetName())
-	flagSet.StringVar(&stringEmptyBucket, WSPortFlag.GetName(), wsPortFlag, WSPortFlag.Usage)
-	
-	// Default values required
-	flagSet.StringVar(&stringEmptyBucket, ethUtils.GCModeFlag.GetName(), ethUtils.GCModeFlag.Value, ethUtils.GCModeFlag.Usage)
-	
-	ctx.Set(ethUtils.GCModeFlag.GetName(), ethUtils.GCModeFlag.Value)
-	
-	return ctx
 }
