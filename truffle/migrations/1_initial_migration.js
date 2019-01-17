02830c35d1ffe5c1e3612736f62029c186a5ea29
ggarri <gabriel.garrido.work@gmail.com>
2019-01-17 13:00:22 +0100

    [NOTASK] Optimize truffle test suite to unlock root account with the minimum overhead logic

--- truffle/migrations/1_initial_migration.js
+++ truffle/migrations/1_initial_migration.js
@@ -1,9 +1,19 @@
-const Migrations = artifacts.require("./Migrations.sol");
+const { waitFor } = require('../test/utils');
 require('dotenv').config({path: `${process.env.PWD}/.env`});
 
-module.exports = (deployer) => {
+const Migrations = artifacts.require("./Migrations.sol");
+
+module.exports = async (deployer) => {
   const pwd = process.env.PASSPHRASE;
   const from = process.env.ROOT_ACCOUNT;
-  web3.personal.unlockAccount(from, pwd, 10000);
+
+  let isUnlock = web3.personal.unlockAccount(from, pwd, 1000);
+  if (!isUnlock) {
+    console.error(`Account ${from} could not be unlock`);
+    process.exit(1);
+  }
+  console.error(`Account ${from} was unlock successfully`);
+  console.log("Deploying `Migrations.sol` ...");
+  await waitFor(1);
   deployer.deploy(Migrations, { from });
 };
