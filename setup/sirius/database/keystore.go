195f758f2867b7af751b93b71b989b7951798d79
Gabriel Garrido Calvo <gabriel.garrido.work@gmail.com>
2019-02-06 11:50:16 +0100

    [#68] Adding new ethereum account for testing proposes (#69)

--- setup/sirius/database/keystore.go
+++ setup/sirius/database/keystore.go
@@ -1,3 +1,6 @@
 package database
 
-var Keystore = map[string]string{}
+var Keystore = map[string]string{
+	"UTC--2019-02-05T19-44-44.676172510Z--d119b8b038d3a67d34ca1d46e1898881626a082b": 
+		`{"address":"d119b8b038d3a67d34ca1d46e1898881626a082b","crypto":{"cipher":"aes-128-ctr","ciphertext":"e4a13511bfa906a4d6342c53e318bb1e3a999cbfb53b4aa91d6d330947324ddb","cipherparams":{"iv":"0e5c4bd0e162623adca5cf6858e8ed99"},"kdf":"scrypt","kdfparams":{"dklen":32,"n":262144,"p":1,"r":8,"salt":"dbda69567a21ed0063fcd05f13f5b18099187952c5a104adaed44144db9d9251"},"mac":"09e2ca8b568c3c6681c5c2487a79412f0ee8d0df0b526edb5ae7626a91860ea5"},"id":"7782128d-8729-4509-bab3-25b67495da99","version":3}`,
+}
