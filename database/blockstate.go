ea009a7cfb3daddff5e50eeb0341d3992200e81e
Lukas Lukac <lukas.lukac.io@gmail.com>
2019-02-12 12:56:39 +0100

    #70 Renames workState to blockState and cleans its methods, adds docs.

--- database/blockstate.go
+++ database/blockstate.go
@@ -16,9 +16,10 @@ import (
 	tmtAbciTypes "github.com/tendermint/tendermint/abci/types"
 )
 
-// The work struct handles block processing.
-// It's updated with each DeliverTx and reset on Commit.
-type workState struct {
+// The blockState struct handles processing of TXs included in a block.
+//
+// It's updated with each ExecuteTx and reset on Persist.
+type blockState struct {
 	header *ethTypes.Header
 	parent *ethTypes.Block
 	state  *state.StateDB
@@ -32,87 +33,77 @@ type workState struct {
 	gp           *core.GasPool
 }
 
-//// nolint: unparam
-//func (ws *workState) accumulateRewards(strategy *coreUtils.Strategy) {
-//	// @Deprecated: Chain Engine is responsible of the rewards
-//}
-
-// Runs ApplyTransaction against the eth blockchain, fetches any logs,
-// and appends the tx, receipt, and logs.
-func (ws *workState) deliverTx(bc *core.BlockChain,
-	config *eth.Config,
-	chainConfig *params.ChainConfig,
-	blockHash common.Hash,
-	tx *ethTypes.Transaction) tmtAbciTypes.ResponseDeliverTx {
-
-	ws.state.Prepare(tx.Hash(), blockHash, ws.txIndex)
+// Executes TX against the eth blockchain state.
+//
+// Fetches TX logs and returns new TX receipt. The changes happen only
+// inside of the Eth state, not disk!
+//
+// Logic copied from `core/state_processor.go` `(p *StateProcessor) Process` that gets
+// normally executed on block persist.
+func (bs *blockState) execTx(bc *core.BlockChain, config *eth.Config, chainConfig *params.ChainConfig, blockHash common.Hash, tx *ethTypes.Transaction) tmtAbciTypes.ResponseDeliverTx {
+	// TODO: Investigate if snapshot should be used `snapshot := bs.state.Snapshot()`
+	bs.state.Prepare(tx.Hash(), blockHash, bs.txIndex)
 	receipt, _, err := core.ApplyTransaction(
 		chainConfig,
 		bc,
 		nil, // defaults to address of the author of the header
-		ws.gp,
-		ws.state,
-		ws.header,
+		bs.gp,
+		bs.state,
+		bs.header,
 		tx,
-		&ws.totalUsedGas,
+		&bs.totalUsedGas,
 		vm.Config{EnablePreimageRecording: config.EnablePreimageRecording},
 	)
 	if err != nil {
-		return tmtAbciTypes.ResponseDeliverTx{
-			Code: tmtCode.CodeTypeEncodingError,
-			Log:  fmt.Sprintf("Got %v", err)}
+		// TODO: investigate if snapshot should be used `bs.state.RevertToSnapshot(snapshot)`
+		return tmtAbciTypes.ResponseDeliverTx{Code: tmtCode.CodeTypeEncodingError, Log: fmt.Sprintf("Error applying state TX %v", err)}
 	}
 
-	logs := ws.state.GetLogs(tx.Hash())
+	logs := bs.state.GetLogs(tx.Hash())
 
-	ws.txIndex++
+	bs.txIndex++
 
-	// The slices are allocated in updateHeaderWithTimeInfo
-	ws.transactions = append(ws.transactions, tx)
-	ws.receipts = append(ws.receipts, receipt)
-	ws.allLogs = append(ws.allLogs, logs...)
+	// The slices are allocated in updateBlockState
+	bs.transactions = append(bs.transactions, tx)
+	bs.receipts = append(bs.receipts, receipt)
+	bs.allLogs = append(bs.allLogs, logs...)
 
 	return tmtAbciTypes.ResponseDeliverTx{Code: tmtAbciTypes.CodeTypeOK}
 }
 
-// Commit the eth ethState, update the header, make a new block and add it to
-// the eth blockchain. The application root hash is the hash of the
-// eth block.
-func (ws *workState) commit(bc *core.BlockChain, db ethdb.Database) (common.Hash, error) {
-
-	// Commit eth ethState and update the header.
-	hashArray, err := ws.state.Commit(false)
+// Persist the eth sate, update the header, make a new block and save it to disk.
+//
+// Returns final application root hash (last block hash).
+func (bs *blockState) persist(bc *core.BlockChain, db ethdb.Database) (common.Hash, error) {
+	hashArray, err := bs.state.Commit(false)
 	if err != nil {
 		return common.Hash{}, err
 	}
-	ws.header.Root = hashArray
+	bs.header.Root = hashArray
 
-	for _, log := range ws.allLogs {
+	for _, log := range bs.allLogs {
 		log.BlockHash = hashArray
 	}
 
-	// Create block object and compute final commit hash (hash of the eth
-	// block).
-	block := ethTypes.NewBlock(ws.header, ws.transactions, nil, ws.receipts)
+	block := ethTypes.NewBlock(bs.header, bs.transactions, nil, bs.receipts)
 	blockHash := block.Hash()
 
-	// Save the block to disk.
-	// log.Info("Committing block", "stateHash", hashArray, "blockHash", blockHash)
+	// Write block to disk
 	_, err = bc.InsertChain([]*ethTypes.Block{block})
 	if err != nil {
-		// log.Info("Error inserting eth block in chain", "err", err)
 		return common.Hash{}, err
 	}
+
 	return blockHash, err
 }
 
-func (ws *workState) updateHeaderWithTimeInfo(
+func (bs *blockState) updateBlockState(
 	config *params.ChainConfig, parentTime uint64, numTx uint64) {
 
-	parentHeader := ws.parent.Header()
-	ws.header.Time = new(big.Int).SetUint64(parentTime)
-	ws.header.Difficulty = ethash.CalcDifficulty(config, parentTime, parentHeader)
-	ws.transactions = make([]*ethTypes.Transaction, 0, numTx)
-	ws.receipts = make([]*ethTypes.Receipt, 0, numTx)
-	ws.allLogs = make([]*ethTypes.Log, 0, numTx)
+	parentHeader := bs.parent.Header()
+	bs.header.Time = new(big.Int).SetUint64(parentTime)
+	bs.header.Difficulty = ethash.CalcDifficulty(config, parentTime, parentHeader)
+	bs.transactions = make([]*ethTypes.Transaction, 0, numTx)
+	bs.receipts = make([]*ethTypes.Receipt, 0, numTx)
+	bs.allLogs = make([]*ethTypes.Log, 0, numTx)
 }
--- database/state.go
+++ database/state.go
@@ -14,22 +14,22 @@ import (
 )
 
 //----------------------------------------------------------------------
-// EthState manages concurrent access to the intermediate workState object
+// EthState manages concurrent access to the intermediate blockState object
 // The eth tx pool fires TxPreEvent in a go-routine,
 // and the miner subscribes to this in another go-routine and processes the tx onto
 // an intermediate ethState. We used to use `unsafe` to overwrite the miner, but this
 // didn't work because it didn't affect the already launched go-routines.
-// So instead we introduce the Pending API in a small commit in go-eth
+// So instead we introduce the Pending API in a small persist in go-eth
 // so we don't even start the miner there, and instead manage the intermediate ethState from here.
-// In the same commit we also fire the TxPreEvent synchronously so the order is preserved,
+// In the same persist we also fire the TxPreEvent synchronously so the order is preserved,
 // instead of using a go-routine.
 
 type EthState struct {
 	ethereum  *eth.Ethereum
 	ethConfig *eth.Config
 
-	mtx  sync.Mutex
-	work workState // latest working ethState
+	mtx        sync.Mutex
+	blockState blockState
 }
 
 // After NewEthState, call SetEthereum and SetEthConfig.
@@ -56,24 +56,15 @@ func (es *EthState) DeliverTx(tx *ethTypes.Transaction) tmtAbciTypes.ResponseDel
 	bc := es.ethereum.BlockChain()
 	chainCfg := es.ethereum.APIBackend.ChainConfig()
 	blockHash := common.Hash{}
-	return es.work.deliverTx(bc, es.ethConfig, chainCfg, blockHash, tx)
+	return es.blockState.execTx(bc, es.ethConfig, chainCfg, blockHash, tx)
 }
 
-//// Accumulate validator rewards.
-//func (es *EthState) AccumulateRewards(strategy *coreUtils.Strategy) {
-//	es.mtx.Lock()
-//	defer es.mtx.Unlock()
-//	// @TODO (ggarri): Pending to define reward strategy
-//	//ethState.eth.Engine().Finalize(ethState.work.ethState, ethState.work.header, []*ethTypes.Header{})
-//	es.work.accumulateRewards(strategy)
-//}
-
-// Commit and reset the work.
+// Commit and reset the blockState.
 func (es *EthState) Commit(receiver common.Address) (common.Hash, error) {
 	es.mtx.Lock()
 	defer es.mtx.Unlock()
 
-	blockHash, err := es.work.commit(es.ethereum.BlockChain(), es.ethereum.ChainDb())
+	blockHash, err := es.blockState.persist(es.ethereum.BlockChain(), es.ethereum.ChainDb())
 	if err != nil {
 		return common.Hash{}, err
 	}
@@ -94,7 +85,6 @@ func (es *EthState) ResetWorkState(receiver common.Address) error {
 }
 
 func (es *EthState) resetWorkState(receiver common.Address) error {
-
 	blockchain := es.ethereum.BlockChain()
 	bcState, err := blockchain.State()
 	if err != nil {
@@ -104,7 +94,7 @@ func (es *EthState) resetWorkState(receiver common.Address) error {
 	currentBlock := blockchain.CurrentBlock()
 	ethHeader := newBlockHeader(receiver, currentBlock)
 
-	es.work = workState{
+	es.blockState = blockState{
 		header:       ethHeader,
 		parent:       currentBlock,
 		state:        bcState,
@@ -121,28 +111,28 @@ func (es *EthState) UpdateHeaderWithTimeInfo(
 	es.mtx.Lock()
 	defer es.mtx.Unlock()
 
-	es.work.updateHeaderWithTimeInfo(config, parentTime, numTx)
+	es.blockState.updateBlockState(config, parentTime, numTx)
 }
 
 func (es *EthState) GasLimit() *core.GasPool {
-	return es.work.gp
+	return es.blockState.gp
 }
 
 //----------------------------------------------------------------------
 // Implements: miner.Pending API (our custom patch to go-eth)
 
-// Return a new block and a copy of the ethState from the latest work.
+// Return a new block and a copy of the ethState from the latest blockState.
 // #unstable
 func (es *EthState) Pending() (*ethTypes.Block, *state.StateDB) {
 	es.mtx.Lock()
 	defer es.mtx.Unlock()
 
 	return ethTypes.NewBlock(
-		es.work.header,
-		es.work.transactions,
+		es.blockState.header,
+		es.blockState.transactions,
 		nil,
-		es.work.receipts,
-	), es.work.state.Copy()
+		es.blockState.receipts,
+	), es.blockState.state.Copy()
 }
 
 // Create a new block header from the previous block.
