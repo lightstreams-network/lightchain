e431bd916bcfb0ad4646e55f0890345ff3c8463c
ggarri <gabriel.garrido.work@gmail.com>
2019-03-06 17:21:49 +0100

    Adding initial mainnet setup for connecting to provisional validator1

--- setup/mainnet/consensus/genesis.go
+++ setup/mainnet/consensus/genesis.go
@@ -2,33 +2,33 @@ package consensus
 
 const Genesis = `
 {
-    "genesis_time": "2019-03-31T00:00:00.000000000Z",
-    "chain_id": "lightstreams-mainnet",
-    "consensus_params": {
-        "block_size": {
-            "max_bytes": "22020096",
-            "max_gas": "-1"
-        },
-        "evidence": {
-            "max_age": "100000"
-        },
-        "validator": {
-            "pub_key_types": [
-                "ed25519"
-            ]
-        }
+  "genesis_time": "2019-03-05T15:45:37.820518684Z",
+  "chain_id": "test-chain-7ovHO6",
+  "consensus_params": {
+    "block_size": {
+      "max_bytes": "22020096",
+      "max_gas": "-1"
     },
-    "validators": [
-        {
-            "address": "34A193CE63F3E51BA875AA42F89E14ACB713D5C1",
-            "pub_key": {
-                "type": "tendermint/PubKeyEd25519",
-                "value": "/axfQYNcqjYjJOTb6jMPNGsbuLD65/JfrEBstfvQXOY="
-            },
-            "power": "10",
-            "name": "mainnet-validator-1"
-        },
-    ],
-    "app_hash": ""
+    "evidence": {
+      "max_age": "100000"
+    },
+    "validator": {
+      "pub_key_types": [
+        "ed25519"
+      ]
+    }
+  },
+  "validators": [
+    {
+      "address": "DF161D1DAFFABF814842B3800A8A150F03D85774",
+      "pub_key": {
+        "type": "tendermint/PubKeyEd25519",
+        "value": "EZg9QOMa05g/cSSjeXLfuwBORT/9ETfIjFxvACxOyMk="
+      },
+      "power": "10",
+      "name": ""
+    }
+  ],
+  "app_hash": ""
 }
 `
