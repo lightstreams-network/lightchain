7a24d531c87c627e24246a9cf6f7cf37a4cd789a
ggarri <gabriel.garrido.work@gmail.com>
2019-03-25 12:46:23 +0100

    #120 Update README documentation to include mainnet instructions | Upgrade version

--- cmd/lightchain/version.go
+++ cmd/lightchain/version.go
@@ -5,15 +5,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const Major = "0"
-const Minor = "12"
-const Fix = "1"
+const Major = "1"
+const Minor = "0"
+const Fix = "0"
 const Verbal = "MainNet"
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Describes version.",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(fmt.Sprintf("Version: %s.%s.%s-alpha %s", Major, Minor, Fix, Verbal))
+		fmt.Println(fmt.Sprintf("Version: %s.%s.%s-beta %s", Major, Minor, Fix, Verbal))
 	},
 }
