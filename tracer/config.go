1dca1ea5b2dac61a889576f9dac324f1ec1c76eb
Gabriel Garrido Calvo <gabriel@lightstreams.io>
2019-05-27 16:58:09 +0200

    #137 Moving tracer.log file into local node datadir (#154)
    * #137 Moving tracer.log file into local node datadir

--- tracer/config.go
+++ tracer/config.go
@@ -1,5 +1,10 @@
 package tracer
 
+import (
+	tmtLog "github.com/tendermint/tendermint/libs/log"
+	"fmt"
+)
+
 type Config struct {
 	ShouldTrace bool
 	LogFilePath string
@@ -7,4 +12,11 @@ type Config struct {
 
 func NewConfig(shouldTrace bool, logFilePath string) Config {
 	return Config{shouldTrace, logFilePath}
+}
+
+func (c Config) PrintWarning(logger tmtLog.Logger) {
+	logger.Info("|--------")
+	logger.Info("| Danger: Tracing enabled is not recommended in production!")
+	logger.Info(fmt.Sprintf("| Tracing output is configured to be persisted at %v", c.LogFilePath))
+	logger.Info("|--------")
 }
