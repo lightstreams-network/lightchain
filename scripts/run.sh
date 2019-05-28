439d767dd22307ec1e47d2be22c689b1e8254bc8
ggarri <gabriel@lightstreams.io>
2019-05-28 14:41:40 +0200

    #133 Enable ethereum debug api when it is requested

--- scripts/run.sh
+++ scripts/run.sh
@@ -40,8 +40,8 @@ DATA_DIR="${DATA_DIR}/${NETWORK}"
 INIT_ARGS="--datadir=${DATA_DIR} --${NETWORK}"
 
 RUN_ARGS="--datadir=${DATA_DIR}"
-RUN_ARGS="${RUN_ARGS} --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpcapi eth,net,web3,personal,admin,debug"
-RUN_ARGS="${RUN_ARGS} --ws --wsport 8556 --wsaddr 0.0.0.0 --wsapi eth,net,web3,personal,admin,debug --wsorigins=*"
+RUN_ARGS="${RUN_ARGS} --rpc --rpcaddr 0.0.0.0 --rpcport 8545 --rpcapi eth,net,web3,personal,debug"
+RUN_ARGS="${RUN_ARGS} --ws --wsaddr 0.0.0.0 --wsport 8556  --wsapi eth,net,web3,personal,debug --wsorigins=*"
 RUN_ARGS="${RUN_ARGS} --tmt_rpc_port=26657 --tmt_p2p_port=26656"
 RUN_ARGS="${RUN_ARGS} --prometheus"
 
